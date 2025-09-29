818dce160452381438163ad6aa9144a8829951cd
Elisaria <120236643+Elisaria@users.noreply.github.com>
2025-09-29 12:27:03 +0800

    Add files via upload

---
+++ Get_Vocabulary.py
@@ -0,0 +1,33 @@
+from keras import layers
+import tensorflow as tf
+
+
+# Add "[cls]" token to each sentence.
+def add_cls_token(line):
+    punctuation_pattern = r"[!\"#$%&'()*+,-./:;<=>?@\[\\\]^_`{|}~—·‘’“”《》【】（），。、；：？！…]"
+    # Remove punctuation marks
+    line = tf.strings.regex_replace(
+        input=line,
+        pattern=punctuation_pattern,
+        rewrite=""
+    )
+    line_with_cls = tf.strings.join([line, "[cls]"], separator=" ")
+    return line_with_cls
+
+
+text_file = 'texts.txt'
+texts = tf.data.TextLineDataset(text_file)
+texts = texts.map(add_cls_token)
+
+# Print the text line by line for viewing
+for line in texts:
+    print(line)
+
+Vectorize_Layer = layers.TextVectorization(output_sequence_length=77)
+# Learn a vocabulary list from the text dataset
+Vectorize_Layer.adapt(texts.batch(64))
+# Print the vocabulary list
+print(Vectorize_Layer.get_vocabulary())
+
+vocabulary = "vocabulary.txt"
+tf.io.write_file(vocabulary, "\n".join(Vectorize_Layer.get_vocabulary()))
